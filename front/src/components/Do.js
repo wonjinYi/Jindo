3c5d8b7d97c13ff69594bfc7c190a498e8c4da13
wonjinYi <studioplug17@gmail.com>
2020-11-26 00:43:52 +0900

    create Do, DoMaker

---
+++ front/src/components/Do.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Do extends React.Component {
+    render() {
+        return (
+            <div className="Do">
+                <p>dodododo</p>
+            </div>
+        );
+    }
+}
+
+export default Do;
---
+++ front/src/components/DoMaker.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class DoMaker extends React.Component {
+    render() {
+        return (
+            <div className="DoMaker">
+                <p>DoooMakerr domakerrr</p>
+            </div>
+        );
+    }
+}
+
+export default DoMaker;
