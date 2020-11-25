7b1d6d9aba9569215c248456b3da21cb932d26a1
wonjinYi <studioplug17@gmail.com>
2020-11-25 22:58:02 +0900

    initial commit front

---
+++ front/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <h1 className="title">Jindo</h1>
+      </header>
+
+      <content>
+        <p>아 lnx 적분하고싶다</p>
+      </content>
+
+      <footer>
+        <p>푸터 영역</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
