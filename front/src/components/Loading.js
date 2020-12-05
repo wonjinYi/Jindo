6e8f1598ac42dd5e222fe3bb583213c346bf4490
wonjinYi <studioplug17@gmail.com>
2020-12-06 02:16:00 +0900

    progress color iinherit

--- front/src/components/Loading.js
+++ front/src/components/Loading.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Backdrop from '@material-ui/core/Backdrop';
 
-
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
@@ -18,7 +17,7 @@ export default function Loading() {
     return (
         <div className="Loading">
             <Backdrop className={classes.backdrop} open={true}>
-                <CircularProgress color="inherit" />
+                <CircularProgress color="inherit" />  
             </Backdrop>
         </div>
     );
