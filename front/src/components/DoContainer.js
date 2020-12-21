360edd6572e06a76b2a34ad7767c6c6153ffc9ed
wonjinYi <studioplug17@gmail.com>
2020-12-21 23:59:35 +0900

    apply style

--- front/src/components/DoContainer.js
+++ front/src/components/DoContainer.js
@@ -1,22 +1,38 @@
 // library, frameworks.
 import React from "react";
 import styled from "styled-components";
-import axios from "axios";
+//components
+import EmptyListNoti from "./components/EmptyListNoti";
 
 export default function DoContainer({setModalInfo, doList}) {
 
 
     return (
-        doList.length > 0
-            ?   (() => {
-                    const children = [];
-                    for (let i = doList.length - 1; i >= 0; i--) {
-                        children.push(<Do key={i} id={doList[i].id} setModalInfo={this.openModal} name={doList[i].name} memo={doList[i].memo} />);
-                    }
-                    return children;
-                })()
-            :   <EmptyListNoti />
-
+        <DoContainer>
+        {
+            doList.length > 0
+                ?   (() => {
+                        const children = [];
+                        for (let i = doList.length - 1; i >= 0; i--) {
+                            children.push(<Do key={i} id={doList[i].id} setModalInfo={this.openModal} name={doList[i].name} memo={doList[i].memo} />);
+                        }
+                        return children;
+                    })()
+                :   <EmptyListNoti />
+        }
+        </DoContainer>
     );
 }
 
+const DoContainer = styled.div`
+    display : flex;
+    flex-direction : column;
+    justify-contents : center;
+    max-width : 50%;
+
+    padding : 10px;
+    margin : 30px 0; 
+    border-radius : 5px;
+
+    background : #ffefa0;
+    `;
