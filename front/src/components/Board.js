5e275804e4c75f23ef867286dc12111b84554381
wonjinYi <studioplug17@gmail.com>
2020-12-25 23:58:50 +0900

    import 3 boards.

--- front/src/components/Board.js
+++ front/src/components/Board.js
@@ -2,6 +2,10 @@
 import React from "react";
 import styled from "styled-components";
 
+import PublicBoard from "./PublicBoard";
+import PrivateBoard from "./PrivateBoard";
+import InfoBoard from "./InfoBoard";
+
 import DoContainer from "./DoContainer";
 
 export default function Board({setModalInfo, doList}) {
