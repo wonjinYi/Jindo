e5b91c32ebe937e4ababc72da6e4974e0a1261c7
wonjinYi <studioplug17@gmail.com>
2021-02-13 04:30:11 +0900

    read works. login works.

--- front/src/components/PrivateBoard.js
+++ front/src/components/PrivateBoard.js
@@ -1,29 +1,44 @@
 // library, frameworks.
-import React from "react";
+import Axios from "axios";
+import { React, useState } from "react";
 //import styled from "styled-components";
 
 import DoContainer from "./DoContainer";
 
-export default function PublicBoard({setModalInfo, doList, sessionID}) {
+export default function PrivateBoard({ setModalInfo, doList }) {
+
+    const [islogin, setIsLogin] = useState(false);
+    updateIsLogin(setIsLogin);
+    console.log(islogin);
 
     return (
-        <div className="PublicBoard">
+        <div className="PrivateBoard">
             {
-                (() => {
-                    if(sessionID != null){
-                        return ( <DoContainer setModalInfo={setModalInfo} doList={doList} /> );
-                    }
-                    else {
-                        return ( 
-                            <a href="https://jindoback.wonj.in/login/daldalso" target="_blank">
-                                <img src="https://daldal.so/media/images/oauth-button.png" />
-                            </a>
-                        );
-                    }
-                })()
+                islogin
+                ?   (<div className="Private-innerwrap">
+                        <a onClick={ () => { handleLogout(setIsLogin) } }> LOGOUT </a>
+                        <DoContainer setModalInfo={setModalInfo} doList={doList} />
+                    </div>)
+                    
+                :   (<a href="https://jindoback.wonj.in/login/daldalso">
+                        <img src="https://daldal.so/media/images/oauth-button.png" />
+                    </a>)
             }
-            
+
         </div>
-        
+
     );
+}
+
+async function updateIsLogin(setIsLogin) {
+    const { data } = await Axios.get('https://jindoback.wonj.in/islogin', {withCredentials : true});
+    setIsLogin(data);
+
+    console.log('updateIsLogin : ', data);
+}
+
+function handleLogout(setIsLogin) {
+    const { data } = Axios.get('https://jindoback.wonj.in/logout', {withCredentials : true});
+    updateIsLogin(setIsLogin);
+    console.log('handleLogout : ', data);
 }
