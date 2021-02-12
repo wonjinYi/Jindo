ff27288c447d29a41ee9e9904a2715ccf86ac63b
wonjinYi <studioplug17@gmail.com>
2021-02-13 02:54:08 +0900

    private route.

--- back/index.js
+++ back/index.js
@@ -22,11 +22,11 @@ dotenv.config();
 const STATE = String(Math.random());
 const sslport = 8000
 
+const SESSION_LIST = [];
 const app = express();
 
 try {
     const option = {
-        //ca : fs.readFileSync('keys/jindoback.wonj.in.ca-bundle.pem'),
         key: fs.readFileSync('keys/jindoback.wonj.in.key.pem'),
         cert: fs.readFileSync('keys/jindoback.wonj.in.crt.pem')
     };
@@ -53,41 +53,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(session({
-	secret:process.env.COOKIE_SECRET,
-	resave:false,
+    secret:process.env.COOKIE_SECRET,
+    resave:false,
     saveUninitialize:true,
     cookie : {
-         httpOnly : false,
-         sameSite:'none',
-         maxAge : 5300000,
-    //     //secure : true,
+        httpOnly : true,
+        sameSite:'none',
+        maxAge : 5300000,
+        secure : true,
     },
-    // name : "session-cookie",
-    // store : new FileStore(),
 }));
 
-app.use('/', (req, res, next) => {
-    console.log("pre session" , req.session.id);
-    next();
-});
-
-/*
-app.use('/', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://wonjinyi.github.io');
-    res.header('Access-Control-Allow-Credentials', true);
-
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    next();
-});*/
 const corsOptions = {
-    origin: true,
-    credentials: true
+    origin : true,
+    credentials : true
 };
 
 app.use(cors(corsOptions));
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.get("/", (req, res, next) => {
+    console.log(req.session.id);
+    console.log(req.session.data)
+})
 app.get("/login/daldalso", (req, res, next) => {
     console.log("LOGIN REQUEST! (Daldalso)");
     res.redirect(`https://daldal.so/oauth/authorize?response_type=code&client_id=${process.env.DALDALSO_CLIENT_ID}&state=${STATE}&redirect_uri=${encodeURIComponent(process.env.DALDALSO_REDIRECT_URL)}`);
@@ -102,47 +91,74 @@ app.get("/login/daldalso/redirect", async (req, res, next) => {
         let token;
         
         respond = await axios.post("https://daldal.so/oauth/token", {
-          client_id: process.env.DALDALSO_CLIENT_ID,
-          client_secret: process.env.DALDALSO_KEY,
-          grant_type: "authorization_code",
-          code: req.query['code']
+            client_id: process.env.DALDALSO_CLIENT_ID,
+            client_secret: process.env.DALDALSO_KEY,
+            grant_type: "authorization_code",
+            code: req.query['code']
         });
         token = respond.data['access_token'];
         // 여기까지 왔다면 인증과정-4 성공
-    
+
         respond = await axios.get("https://daldal.so/oauth/api/me", {
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
         });
         console.log(respond.data);
+
         req.session.islogin = true;
-        console.log(req.session.islogin);
-        // 여기까지 왔다면 인증과정-6 성공
-      }catch(e){
-        console.error(e);
-        res.sendStatus(500);
-    }
+        SESSION_LIST.push({
+            id : req.session.id, 
+            namd : respond.data.name,
+            email : respond.data.account,
+        });
+
+        console.log(SESSION_LIST);
+            // 여기까지 왔다면 인증과정-6 성공
+        } catch(e) {
+            console.error(e);
+            res.sendStatus(500);
+        }
     
     res.redirect("http://localhost:3000/")
-    //res.send('dfdfdfdfdf');
 })
 
 app.post('/sessiontest', function (req, res, next) {
-    console.log(req.session);
     console.log(req.session.id);
-    console.log(req.session.num == undefined);
-    if(req.session == undefined){
+    console.log(req.session.islogin);
+    if(req.session.num == undefined){
         req.session.num = 1;
-        console.log(req.session.num);
     } else {
         req.session.num =  req.session.num + 1;
         console.log(req.session.num);
     }
-    console.log(req.session);
+    console.log(req.session.num);
+
+    res.send(req.session.id);
+});
+
+app.get('/islogin', function (req, res, next) {
     console.log(req.session.id);
-    res.send('ghi');
+    console.log(req.session.islogin);
+    if( req.session.islogin === true ){
+        res.send(true);
+    }
+    else {
+        res.send(false);
+    }
+    
 })
+app.get('/logout', function (req, res, next) {
+    console.log(req.session.id);
+    req.session.destroy( function(req, res, next) {
+        //console.log(req.session.id);
+        console.log(' ㄴ logout !');
+    });
+
+    res.send('logout done')
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.get("/public/data", async (req, res, next) => {
     const read = await PublicBoard.findAll({
@@ -195,6 +211,78 @@ app.post("/public/delete", async (req, res, next) => {
 
     res.json(del);
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// login check
+app.use("/private", (req, res, next) => {
+    const target = SESSION_LIST.find( el => (el.id === req.session.id) );
+    if ( target === -1 ) {
+        res.send('need login');
+    } else {
+        next();
+    }
+})
+
+app.get("/private/read", async (req, res) => {
+    const read = await PrivateBoard.findAll({
+        attributes : ['id', 'name', 'memo', 'updatedAt']
+    });
+
+    console.log("--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--")
+    console.log("[PRIVATE] Read DONE");
+    console.log("--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--")
+
+    res.json(read);
+})
+
+app.post("/private/create", async (req, res, next) => {
+    // 현재 요청이 들어온 세션이 SESSION_LIST에서 몇 번째인지 찾기 (인덱스)
+    const target = SESSION_LIST.find( el => (el.id === req.session.id) );
+    if ( target === -1 ) {
+        res.send('need login');
+    }
+
+    const create = await PrivateBoard.create({
+        name : SESSION_LIST[target].name,
+        email : SESSION_LIST[target].email,
+        memo : req.body.memo,
+    });
+
+    console.log("--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--")
+    console.log(`[PRIVATE] Create DONE\n  ㄴ name : ${SESSION_LIST[target].name}    email : ${SESSION_LIST[target].email}    memo : ${req.body.memo}`);
+    console.log("--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--")
+
+    res.json(create);
+});
+
+app.post("/private/edit", async (req, res, next) => {
+    const edit = await PrivateBoard.update({
+        memo : req.body.memo,
+    }, {
+        where : { id : req.body.id },
+    });
+
+    console.log("--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--")
+    console.log(`[PRIVATE] Edit DONE\n  ㄴ name : ${SESSION_LIST[target].name}    email : ${SESSION_LIST[target].email}    memo : ${req.body.memo}`);
+    console.log("--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--")
+
+    res.json(edit);
+});
+
+app.post("/private/delete", async (req, res, next) => {
+    const del = await PrivateBoard.destroy({
+        where : { id : req.body.id },
+    });
+
+    console.log("--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--")
+    console.log(`[PRIVATE] Delete DONE\n  ㄴ id : ${req.body.id}`);
+    console.log("--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--ㅇ--")
+
+    res.json(del);
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.use("/", (req, res, next) => {
     res.send("여기가 어디지? 주변을 둘러보니 순백의 공허만이 가득했다")
